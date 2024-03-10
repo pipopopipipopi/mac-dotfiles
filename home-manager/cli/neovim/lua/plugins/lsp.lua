3cfabb5802aefa6b3ca71cf9dddb702075d9fa78
pipopo <puchi.maru.milk@gmail.com>
2024-03-10 15:12:54 +0900

    install nvim-cmp

---
+++ home-manager/cli/neovim/lua/plugins/lsp.lua
@@ -0,0 +1,59 @@
+return {
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            { "neovim/nvim-lspconfig" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "hrsh7th/cmp-cmdline" },
+
+            { "L3MON4D3/LuaSnip" },
+            { "saadparwaiz1/cmp_luasnip" }
+        },
+        event = "VimEnter",
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                })
+            })
+
+            cmp.setup.cmdline({ "/", "?" }, {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "buffer" }
+                })
+            })
+
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" }
+                }, {
+                        { name = "cmdline" }
+                    })
+            })
+
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        end,
+    }
+}
