a647b2e7e23f1e630f8466a58a63f0c20e192d9d
pipopo <puchi.maru.milk@gmail.com>
2024-03-12 22:22:57 +0900

    edit toggleterm settings

--- home-manager/cli/neovim/lua/plugins/toggleterm.lua
+++ home-manager/cli/neovim/lua/plugins/toggleterm.lua
@@ -5,5 +5,18 @@ return {
         require("toggleterm").setup({
             shade_terminales = false,
         })
+
+        local Terminal = require("toggleterm.terminal").Terminal
+        local gitui = Terminal:new({
+            cmd = "gitui",
+            direction = "float",
+            hidden = true,
+        })
+
+        function _gitui_toggle()
+            gitui:toggle()
+        end
+
+        vim.keymap.set("n", "<Space>gu", ":lua _gitui_toggle()<cr>", { noremap = true, silent = true })
     end,
 }
