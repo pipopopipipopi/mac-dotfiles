c35fa9d4d3434121056faef5e700f27130af74f9
pipopo <puchi.maru.milk@gmail.com>
2024-03-12 10:51:07 +0900

    edit neovim options

--- home-manager/cli/neovim/lua/config/options.lua
+++ home-manager/cli/neovim/lua/config/options.lua
@@ -1,41 +1,61 @@
-vim.opt.encoding = 'utf-8';
-vim.opt.fileencoding = 'utf-8'
-vim.opt.title = true
-vim.opt.backup = false
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.cmdheight = 2
-vim.opt.completeopt = { 'menuone', 'noselect' }
-vim.optconceallevel = 0
-vim.opt.hlsearch = true
-vim.opt.ignorecase = true
-vim.opt.mouse = 'a'
-vim.opt.pumheight = 10
-vim.opt.showmode = false
-vim.opt.showtabline = 2
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.swapfile = false
-vim.opt.termguicolors = true
-vim.opt.timeoutlen = 300
-vim.opt.undofile = true
-vim.opt.updatetime = 300
-vim.opt.writebackup = false
-vim.opt.shell = 'fish'
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.cursorline = false
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.numberwidth = 4
-vim.opt.wrap = true
-vim.opt.winblend = 30
-vim.opt.wildoptions = 'pum'
-vim.opt.virtualedit = 'onemore'
-vim.opt.pumblend = 30
-vim.opt.background = 'dark'
+-- Character code
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- Scroll
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
-vim.opt.guifont = 'Hack Nerd Font Regular:h10'
+
+-- Row
+vim.opt.number = true
+vim.opt.numberwidth = 4
+vim.opt.relativenumber = true
+vim.opt.cursorline = false
+
+-- Tab, Indent
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 4
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+-- UI
+vim.opt.cmdheight = 0
+vim.opt.showtabline = 2
+vim.opt.laststatus = 3
+vim.opt.termguicolors = true
+vim.opt.title = true
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.optconceallevel = 0
+vim.opt.pumheight = 10
+vim.opt.showmode = false
+vim.opt.wrap = true
+vim.opt.wildoptions = "pum"
+vim.opt.winblend = 30
+vim.opt.pumblend = 30
+vim.opt.background = "dark"
+vim.opt.guifont = "Hack Nerd Font Regular:h10"
+
+-- File
+vim.opt.backup = false
+vim.opt.hidden = true
+vim.opt.swapfile = false
+vim.opt.updatetime = 300
+vim.opt.undofile = true
+vim.opt.writebackup = false
+
+-- Misc
+vim.opt.clipboard = "unnamedplus"
+vim.opt.mouse = "a"
+vim.opt.timeoutlen = 300
+vim.opt.shell = "fish"
+vim.opt.virtualedit = "onemore"
 vim.opt.splitbelow = false -- オンのとき、ウィンドウを横分割すると新しいウィンドウはカレントウィンドウの下に開かれる
 vim.opt.splitright = false -- オンのとき、ウィンドウを縦分割すると新しいウィンドウはカレントウィンドウの右に開かれる
