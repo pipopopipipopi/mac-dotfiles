df6fa0144352a1af51df605db006bc40ea5612fa
pipopo <puchi.maru.milk@gmail.com>
2024-03-13 10:42:35 +0900

    edit neovim settings

--- home-manager/cli/neovim/lua/plugins/lsp.lua
+++ home-manager/cli/neovim/lua/plugins/lsp.lua
@@ -34,7 +34,62 @@ return {
             { "hrsh7th/cmp-cmdline" },
 
             { "L3MON4D3/LuaSnip" },
-            { "saadparwaiz1/cmp_luasnip" }
+            { "saadparwaiz1/cmp_luasnip" },
+            
+            {
+                "onsails/lspkind-nvim",
+                config = function()
+                    require('lspkind').init({
+                        -- DEPRECATED (use mode instead): enables text annotations
+                        --
+                        -- default: true
+                        -- with_text = true,
+
+                        -- defines how annotations are shown
+                        -- default: symbol
+                        -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+                        mode = 'symbol_text',
+
+                        -- default symbol map
+                        -- can be either 'default' (requires nerd-fonts font) or
+                        -- 'codicons' for codicon preset (requires vscode-codicons font)
+                        --
+                        -- default: 'default'
+                        preset = 'codicons',
+
+                        -- override preset symbols
+                        --
+                        -- default: {}
+                        symbol_map = {
+                        Text = "󰉿",
+                        Method = "󰆧",
+                        Function = "󰊕",
+                        Constructor = "",
+                        Field = "󰜢",
+                        Variable = "󰀫",
+                        Class = "󰠱",
+                        Interface = "",
+                        Module = "",
+                        Property = "󰜢",
+                        Unit = "󰑭",
+                        Value = "󰎠",
+                        Enum = "",
+                        Keyword = "󰌋",
+                        Snippet = "",
+                        Color = "󰏘",
+                        File = "󰈙",
+                        Reference = "󰈇",
+                        Folder = "󰉋",
+                        EnumMember = "",
+                        Constant = "󰏿",
+                        Struct = "󰙅",
+                        Event = "",
+                        Operator = "󰆕",
+                        TypeParameter = "",
+                        },
+                    })
+                end,
+            }
         },
         config = function()
             local cmp = require("cmp")
